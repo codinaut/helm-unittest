33b81521085325c2e3c52d50b45825c815f4d04d
Cody Lee <cody.lee@datadoghq.com>
2020-05-22 12:47:58 -0500

    just use make and avoid github bouncing

--- install-binary.sh
+++ install-binary.sh
@@ -20,77 +20,11 @@ if [[ $SKIP_BIN_INSTALL == "1" ]]; then
   exit
 fi
 
-# initArch discovers the architecture for this system.
-initArch() {
-  ARCH=$(uname -m)
-  case $ARCH in
-    armv5*) ARCH="armv5";;
-    armv6*) ARCH="armv6";;
-    armv7*) ARCH="armv7";;
-    aarch64) ARCH="arm64";;
-    x86) ARCH="386";;
-    x86_64) ARCH="amd64";;
-    i686) ARCH="386";;
-    i386) ARCH="386";;
-  esac
-}
-
-# initOS discovers the operating system for this system.
-initOS() {
-  OS=$(echo `uname`|tr '[:upper:]' '[:lower:]')
-
-  case "$OS" in
-    # Msys support
-    msys*) OS='windows';;
-    # Minimalist GNU for Windows
-    mingw*) OS='windows';;
-    darwin) OS='macos';;
-  esac
-}
-
-# verifySupported checks that the os/arch combination is supported for
-# binary builds.
-verifySupported() {
-  local supported="linux-amd64\nmacos-amd64\nwindows-amd64"
-  if ! echo "${supported}" | grep -q "${OS}-${ARCH}"; then
-    echo "No prebuild binary for ${OS}-${ARCH}."
-    exit 1
-  fi
-
-  if ! type "curl" >/dev/null 2>&1 && ! type "wget" >/dev/null 2>&1; then
-    echo "Either curl or wget is required"
-    exit 1
-  fi
-}
-
-# getDownloadURL checks the latest available version.
-getDownloadURL() {
-  local version=$(git describe --tags --exact-match 2>/dev/null)
-  DOWNLOAD_URL="https://github.com/$PROJECT_GH/archive/$version.tar.gz"
-}
-
-# downloadFile downloads the latest binary package and also the checksum
-# for that binary.
-downloadFile() {
-  PLUGIN_TMP_FILE="/tmp/${PROJECT_NAME}.tar.gz"
-  echo "Downloading $DOWNLOAD_URL"
-  if type "curl" >/dev/null 2>&1; then
-    curl -L "$DOWNLOAD_URL" -o "$PLUGIN_TMP_FILE"
-  elif type "wget" >/dev/null 2>&1; then
-    wget -q -O "$PLUGIN_TMP_FILE" "$DOWNLOAD_URL"
-  fi
-}
-
 # installFile verifies the SHA256 for the file, then unpacks and
 # installs it.
 installFile() {
-  HELM_TMP="/tmp/$PROJECT_NAME"
-  mkdir -p "$HELM_TMP"
-  tar xf "$PLUGIN_TMP_FILE" -C "$HELM_TMP"
-  HELM_TMP_BIN="$HELM_TMP/untt"
   echo "Preparing to install into ${HELM_PLUGIN_PATH}"
-  # Use * to also copy the file withe the exe suffix on Windows
-  cp "$HELM_TMP_BIN"* "$HELM_PLUGIN_PATH"
+  make install
   echo "$PROJECT_NAME installed into $HELM_PLUGIN_PATH"
 }
 
@@ -117,10 +51,5 @@ testVersion() {
 #Stop execution on any error
 trap "fail_trap" EXIT
 set -e
-initArch
-initOS
-verifySupported
-getDownloadURL
-downloadFile
 installFile
 testVersion
